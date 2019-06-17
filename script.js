639f5adf34d14d12deca790a19e0ff39559bf162
Vanessa <vanmtv@hotmail.com>
2019-06-17 15:24:58 -0300

    Iniciar js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-let quadro = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+let blocos = document.querySelectorAll("button");
 
-function montarQuadro(){
-    let linha1 = [quadro[0], quadro[1], quadro[2]]
-    let linha2 = [quadro[3], quadro[4], quadro[5]]
-    let linha3 = [quadro[6], quadro[7], quadro[8]]
+function adicionarX(){
+    this.appendChild('X');
 }
+
+function adicionarO(){
+    this.appendChild('O');
+}
+
 
