526b0461af7e915931ce8cb1a3308203e1f4a06c
Jeferson Amorim <jeferson.amorim@live.com>
2019-06-17 15:19:31 -0300

    Criar layout do jogo da velha

--- script.js
+++
@@ -1,8 +0,0 @@
-let quadro = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-
-function montarQuadro(){
-    let linha1 = [quadro[0], quadro[1], quadro[2]]
-    let linha2 = [quadro[3], quadro[4], quadro[5]]
-    let linha3 = [quadro[6], quadro[7], quadro[8]]
-}
-
